fb12f4e8ada927a2299ec52f8979445064408102
chrissx <chrissx@chrissx.de>
2021-02-23 19:01:45 +0100

    using macports without root - unfortunately - kinda is out of question rn

--- install.sh
+++ install.sh
@@ -15,5 +15,5 @@ MACPORTS='https://distfiles.macports.org/MacPorts/MacPorts-2.6.4_1-11-BigSur.pkg
 sudo chown -R $USER /opt/local
 
 brew install $(cat packages.brew)
-port install $(cat packages.brew)
+sudo port install $(cat packages.brew)
 pip3 install --user $(cat packages.pip3)
