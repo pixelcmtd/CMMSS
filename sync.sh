47221fba702f6ee11ef7d823f4eae80bf85020a1
pixel <chrissx@chrissx.de>
2022-03-21 02:10:42 +0100

    a script for syncing my stuff

---
+++ sync.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -uxe
+
+# copy configs
+cp -f ~/.zshrc zshrc
+cp -f ~/.zshtheme zshtheme
+cp -f ~/.vimrc vimrc
+cp -f ~/.env env
+cp -f ~/.editorconfig editorconfig
+
+# generate package lists for homebrew and mas
+brew list > packages.brew
+cargo install --list | grep -v '^ ' | cut -d' ' -f1 | sort > packages.cargo
+mas list | sort > packages.mas
+
+set +x
+echo "Have fun with hours of git diff and editing the package lists!"
